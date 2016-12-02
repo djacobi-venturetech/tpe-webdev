f6128260e9a96898f9ad4a3bac614ad4c6d69095
djacobi-venturetech <djacobi@venturetech.net>
2016-12-02 14:24:42 -0600

    Inline error scripts

--- web/src/javascript/Entropy/templates/common-scripts.js
+++ web/src/javascript/Entropy/templates/common-scripts.js
@@ -134,6 +134,64 @@ jQuery(function () {
 
 	setupExpandCollapse();
 
+	/**
+	 * Created by vtdev on 4/7/16.
+	 */
+	var CN_HAS_ERROR = "has-error";
+
+	function errorMessageCleanup(){
+		$("." + CN_HAS_ERROR).each(function (idx, el) {
+			var $el = $(el);
+			$el.removeClass(CN_HAS_ERROR);
+			$el.find('.error-message').remove();
+		});
+	}
+
+	function setupErrorMessages(ctx){
+		function createWrapper($prop){
+			var $it;
+			if($prop.hasClass('ctb') || $prop.hasClass('rtb'))
+				$it = $prop.find('label').wrap('<span class="msg-wrap"></span>').parent();
+			else
+				$it = $prop.wrap("<span></span>");
+			return $it;
+		}
+		var $ctx = $(ctx || document), hasErrors;
+		var $mcs = $ctx.hasClass('message-container') ? $ctx : $ctx.find('.message-container');
+		if($mcs.length === 0)
+			return;
+		$mcs.each(function (idx, mc) {
+			var $mc = $(mc);
+			$mc.find('.error [data-source]').each(function (idx, el) {
+				var $el = $(el), id = $el.data('source');
+				var $prop = $('#' + id);
+				if($prop.length === 0)
+					return;
+				if($prop.prop('tagName').toLowerCase() != 'div') {
+					var $it = createWrapper($prop);
+					$it.addClass(CN_HAS_ERROR);
+					$it.append('<span class="error-message"><span class="error-text">' + $el.text() + '</span></span>');
+					hasErrors = true;
+				} else {
+					$prop.addClass(CN_HAS_ERROR);
+					$prop.append('<div class="error-message"><span class="error-text">' + $el.text() + '</span></div>');
+					hasErrors = true;
+				}
+				$el.parent().remove();
+			});
+		});
+		if(hasErrors) {
+			$mcs.append('<div class="message error"><span class="brief">Please review the errors below</span></div>');
+			//noinspection JSUnresolvedFunction - defined in MIWT util.js
+			var scrollTo = $('.error-message').parent().first()[0];
+			setTimeout(function(){
+				scrollTo.scrollIntoViewIfNeeded(true);
+			}, 50);
+		}
+	}
+
+	setupErrorMessages();
+
 	$('form.miwt-form').each(function (idx, form) {
 		form.submit_options = {
 			preProcessNode: function (data) {
@@ -141,12 +199,16 @@ jQuery(function () {
 				return data.content;
 			},
 			postProcessNode: function (data) {
-				$.each(data, function (idx, d) {
+				$.each(data, function (idx, d, ctx) {
 					initSelect2(d.node);
 					handleDataDownload(d.node);
 					enableTooltips(d.node);
 					setupExpandCollapse(d.node);
 					switchSupport(d.node);
+					errorMessageCleanup();
+					setTimeout(function () {
+						setupErrorMessages(ctx);
+					}, 1);
 				});
 			},
 			postUpdate: function () {
